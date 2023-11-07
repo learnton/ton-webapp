f9d11611c552870d82986a6e499d12851d8d2fca
refined-x <tower1229@foxmail.com>
2023-11-07 15:32:25 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Hello daisyUI</button>
+    </>
+  );
+}
+
+export default App;
