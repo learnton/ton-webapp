aa8ad6ce01d49e38b04d3cb448bf0cefb3c946a7
refined-x <tower1229@foxmail.com>
2023-11-23 08:38:45 +0800

    fix: type err

--- src/utils/indexdb/cache-template.ts
+++ src/utils/indexdb/cache-template.ts
@@ -1,10 +1,10 @@
 // Copyright 2021-2023 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { HexString } from '@zcloak/crypto/types';
-import { DidUrl } from '@zcloak/did-resolver/types';
+import { HexString } from "@zcloak/crypto/types";
+import { DidUrl } from "@zcloak/did-resolver/types";
 
-import { ApplicationProps, CARD_TYPE } from '@zkid-wallet/react';
+import { ApplicationProps, CARD_TYPE } from "@/components";
 
 export interface CacheTemplate {
   id: number;
